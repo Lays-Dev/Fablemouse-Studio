8417aa1dd41da57d42f0ee67ee16c5c71d17dec9
DavFlo05 <davflor155@gmail.com>
2026-09-25 01:05:20 -0400

    Add Player Sprint

--- Unity File/Make Me Forget/Assets/Scripts/Player/PlayerMovement.cs
+++ Unity File/Make Me Forget/Assets/Scripts/Player/PlayerMovement.cs
@@ -3,30 +3,17 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Controls how fast the player moves
+    // Controls how fast the player moves.
     [SerializeField] private float _moveSpeed = 5f;
 
+    // Controls how fast the player moves while sprinting.
+    [SerializeField] private float _sprintSpeed = 8f;
+
     private Vector2 _moveInput;
+    private bool _isSprinting;
     private Rigidbody _rigidbody;
 
-    // Gets the player's movement input.
-    public void OnMove(InputValue value)
-    {
-        _moveInput = value.Get<Vector2>();
-    }
-
-    //Moves the player left and right
-    private void FixedUpdate()
-    {
-        if (_rigidbody == null)
-        {
-            return;
-        }
-        Vector3 velocity = _rigidbody.linearVelocity;
-        velocity.x = _moveInput.x * _moveSpeed;
-        _rigidbody.linearVelocity = velocity;
-    }
-    // Gets the Rigidbody attached to the player
+    // Gets the Rigidbody attached to the player.
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody>();
@@ -36,15 +23,38 @@ public class PlayerMovement : MonoBehaviour
             Debug.LogError("Player needs a Rigidbody.");
         }
     }
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    // Moves the player left and right.
+    private void FixedUpdate()
     {
-        
+        if (_rigidbody == null)
+        {
+            return;
+        }
+
+        float currentSpeed = _moveSpeed;
+
+        if (_isSprinting)
+        {
+            currentSpeed = _sprintSpeed;
+        }
+
+        Vector3 velocity = _rigidbody.linearVelocity;
+
+        velocity.x = _moveInput.x * currentSpeed;
+
+        _rigidbody.linearVelocity = velocity;
     }
 
-    // Update is called once per frame
-    void Update()
+    // Gets the player's movement input.
+    public void OnMove(InputValue value)
     {
-        
+        _moveInput = value.Get<Vector2>();
+    }
+
+    // Gets the player's sprint input.
+    public void OnSprint(InputValue value)
+    {
+        _isSprinting = value.isPressed;
     }
 }
